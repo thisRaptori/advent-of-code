aed5a49b76e2a32968e61ad3e737729d9ae5efcb
Joe McGrath <joe@raptori.dev>
2019-12-02 05:42:58 +0000

    correct terminology

--- advent-of-code/2019/3.js
+++ advent-of-code/2019/3.js
@@ -1,37 +1,37 @@
-export function compute(...operations) {
+export function compute(...memory) {
     const state = {
-        operations,
-        position: 0,
+        memory,
+        pointer: 0,
     }
 
     do {
-        const { position } = state
-        const keys = [
-            state.operations[position + 1],
-            state.operations[position + 2],
-            state.operations[position + 3],
+        const { pointer } = state
+        const addresses = [
+            state.memory[pointer + 1],
+            state.memory[pointer + 2],
+            state.memory[pointer + 3],
         ]
-        const values = keys.map(key => state.operations[key])
+        const parameters = addresses.map(key => state.memory[key])
 
-        switch (state.operations[state.position]) {
+        switch (state.memory[state.pointer]) {
             case 1: {
-                state.operations[keys[2]] = values[0] + values[1]
-                state.position += 4
+                state.memory[addresses[2]] = parameters[0] + parameters[1]
+                state.pointer += 4
                 break;
             }
 
             case 2: {
-                state.operations[keys[2]] = values[0] * values[1]
-                state.position += 4
+                state.memory[addresses[2]] = parameters[0] * parameters[1]
+                state.pointer += 4
                 break;
             }
 
             case 99: {
-                console.log(JSON.stringify(state.operations));
-                return state.operations;
+                console.log(JSON.stringify(state.memory));
+                return state.memory;
             }
         }
-    } while (state.position < state.operations.length)
+    } while (state.pointer < state.memory.length)
 
-    return state.operations
+    return state.memory
 }
