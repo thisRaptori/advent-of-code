6e3d43a0121eded96279fd6d104dd149ce4fc25c
Joe McGrath <joe@raptori.dev>
2019-12-02 01:13:19 +0000

    advent of code 2019 part 1

---
+++ advent-of-code/2019/1.js
@@ -0,0 +1,10 @@
+function calculateRequiredFuel(mass) {
+    return Math.floor(mass / 3) - 2
+}
+
+export function countFuel(...modules) {
+    return modules.reduce(
+        (acc, cur) => (acc + calculateRequiredFuel(cur)),
+        0
+    )
+}
