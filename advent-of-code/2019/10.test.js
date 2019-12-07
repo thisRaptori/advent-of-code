88331e60a87550279ac9d3ea2eac170d023ef091
Joe McGrath <joe@raptori.dev>
2019-12-07 15:36:41 +0000

    clean up tests

--- advent-of-code/2019/10.test.js
+++ advent-of-code/2019/10.test.js
@@ -28,79 +28,16 @@ describe("compute", () => {
     expect(jumpI(1)).toBe(1);
     expect(jumpI(0)).toBe(0);
 
-    const jumpP = compute(
-      3,
-      12,
-      6,
-      12,
-      15,
-      1,
-      13,
-      14,
-      13,
-      4,
-      13,
-      99,
-      -1,
-      0,
-      1,
-      9
-    );
+    // prettier-ignore
+    const jumpP = compute(3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9);
     expect(jumpP(10)).toBe(1);
     expect(jumpP(1)).toBe(1);
     expect(jumpP(0)).toBe(0);
   });
 
   it("should work for the larger example", () => {
-    const compareToEight = compute(
-      3,
-      21,
-      1008,
-      21,
-      8,
-      20,
-      1005,
-      20,
-      22,
-      107,
-      8,
-      21,
-      20,
-      1006,
-      20,
-      31,
-      1106,
-      0,
-      36,
-      98,
-      0,
-      0,
-      1002,
-      21,
-      125,
-      20,
-      4,
-      20,
-      1105,
-      1,
-      46,
-      104,
-      999,
-      1105,
-      1,
-      46,
-      1101,
-      1000,
-      1,
-      20,
-      4,
-      20,
-      1105,
-      1,
-      46,
-      98,
-      99
-    );
+    // prettier-ignore
+    const compareToEight = compute(3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99);
     expect(compareToEight(7)).toBe(999);
     expect(compareToEight(8)).toBe(1000);
     expect(compareToEight(9)).toBe(1001);
--- advent-of-code/2019/5.test.js
+++ advent-of-code/2019/5.test.js
@@ -980,31 +980,10 @@ describe("walkPath", () => {
 describe("findIntersections", () => {
   const path1 = ["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"];
   const path2 = ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"];
-  const path3 = [
-    "R98",
-    "U47",
-    "R26",
-    "D63",
-    "R33",
-    "U87",
-    "L62",
-    "D20",
-    "R33",
-    "U53",
-    "R51"
-  ];
-  const path4 = [
-    "U98",
-    "R91",
-    "D20",
-    "R16",
-    "D67",
-    "R40",
-    "U7",
-    "R15",
-    "U6",
-    "R7"
-  ];
+  // prettier-ignore
+  const path3 = ["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"];
+  // prettier-ignore
+  const path4 = ["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"];
 
   it("should return an array containing the path intersections", () => {
     expect(findIntersections(path1, path2)).toEqual([
@@ -1026,31 +1005,10 @@ describe("findIntersections", () => {
 describe("findClosestIntersection", () => {
   const path1 = ["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"];
   const path2 = ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"];
-  const path3 = [
-    "R98",
-    "U47",
-    "R26",
-    "D63",
-    "R33",
-    "U87",
-    "L62",
-    "D20",
-    "R33",
-    "U53",
-    "R51"
-  ];
-  const path4 = [
-    "U98",
-    "R91",
-    "D20",
-    "R16",
-    "D67",
-    "R40",
-    "U7",
-    "R15",
-    "U6",
-    "R7"
-  ];
+  // prettier-ignore
+  const path3 = ["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"];
+  // prettier-ignore
+  const path4 = ["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"];
 
   it("should return the closest intersection", () => {
     expect(findClosestIntersection(path1, path2)).toEqual(159);
